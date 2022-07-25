98a970bc0813784cd750d6dfc61e31a16da9d8b7
Julien DÉCHARNE <jdecharne@youwol.com>
2022-07-25 15:37:17 +0200

    🏗️ build docker image from submodule py-youwol

--- src/config_prod.py
+++ src/config_prod.py
@@ -1,24 +1,27 @@
 import os
 
-from config_common import on_before_startup, cache_prefix
-
+from config_common import on_before_startup
 from youwol_assets_gateway import Configuration
-
-from youwol_utils import AuthClient, CacheClient, CdnClient, get_headers_auth_admin_from_env
+from youwol_utils import CdnClient, RedisCacheClient, get_authorization_header
 from youwol_utils.clients.assets.assets import AssetsClient
 from youwol_utils.clients.data_api.data import get_remote_storage_client, get_remote_docdb_client, DataClient
 from youwol_utils.clients.files import FilesClient
 from youwol_utils.clients.flux.flux import FluxClient
+from youwol_utils.clients.oidc.oidc_config import PrivateClient, OidcInfos
 from youwol_utils.clients.stories.stories import StoriesClient
 from youwol_utils.clients.treedb.treedb import TreeDbClient
 from youwol_utils.context import DeployedContextReporter
-from youwol_utils.middlewares import Middleware
+from youwol_utils.middlewares import AuthMiddleware, JwtProviderCookie
 from youwol_utils.servers.fast_api import FastApiMiddleware, AppConfiguration, ServerOptions
 
 
 async def get_configuration():
-
-    required_env_vars = ["AUTH_HOST", "AUTH_CLIENT_ID", "AUTH_CLIENT_SECRET", "AUTH_CLIENT_SCOPE"]
+    required_env_vars = [
+        "OPENID_BASE_URL",
+        "OPENID_CLIENT_ID",
+        "OPENID_CLIENT_SECRET",
+        "REDIS_HOST"
+    ]
 
     not_founds = [v for v in required_env_vars if not os.getenv(v)]
     if not_founds:
@@ -41,9 +44,13 @@ async def get_configuration():
     assets_client = AssetsClient(url_base="http://assets-backend")
     files_client = FilesClient(url_base="http://files-backend")
 
-    openid_host = os.getenv("AUTH_HOST")
-    auth_client = AuthClient(url_base=f"https://{openid_host}/auth")
-    cache_client = CacheClient(host="redis-master.infra.svc.cluster.local", prefix=cache_prefix)
+    openid_infos = OidcInfos(
+        base_uri=(os.getenv("OPENID_BASE_URL")),
+        client=PrivateClient(
+            client_id=(os.getenv("OPENID_CLIENT_ID")),
+            client_secret=(os.getenv("OPENID_CLIENT_SECRET"))
+        )
+    )
 
     async def _on_before_startup():
         await on_before_startup(service_config)
@@ -56,7 +63,7 @@ async def get_configuration():
         treedb_client=treedb_client,
         assets_client=assets_client,
         files_client=files_client,
-        admin_headers=await get_headers_auth_admin_from_env()
+        admin_headers=await get_authorization_header(openid_infos)
     )
 
     server_options = ServerOptions(
@@ -65,11 +72,18 @@ async def get_configuration():
         base_path="",
         middlewares=[
             FastApiMiddleware(
-                Middleware, {
-                    "auth_client": auth_client,
-                    "cache_client": cache_client,
-                    # healthz need to not be protected as it is used for liveness prob
-                    "unprotected_paths": lambda url: url.path.split("/")[-1] == "healthz"
+                AuthMiddleware, {
+                    'openid_infos': openid_infos,
+                    'predicate_public_path': lambda url:
+                    url.path.endswith("/healthz"),
+                    'jwt_providers': [
+                        JwtProviderCookie(
+                            jwt_cache=RedisCacheClient(
+                                host=(os.getenv("REDIS_HOST")),
+                                prefix='jwt_cache'
+                            ),
+                            openid_infos=openid_infos
+                        )],
                 }
             )
         ],
