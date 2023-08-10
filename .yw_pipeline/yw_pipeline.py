84885eb55485c2c170b68e951443b19a6eafa79f
youwol-guillaume <greinisch@youwol.com>
2023-08-10 07:30:36 +0200

    :wrench: `yw_pipeline.py` => sync w/ new py-youwol API

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,13 +1,16 @@
-from youwol.environment.models import IPipelineFactory
-from youwol.environment.youwol_environment import YouwolEnvironment
-from youwol.pipelines.pipeline_fastapi_youwol_backend import get_backend_apps_yw_pipeline
-from youwol_utils.context import Context
+from youwol.app.environment.models import IPipelineFactory
+from youwol.app.environment.youwol_environment import YouwolEnvironment
+from youwol.pipelines.pipeline_fastapi_youwol_backend import (
+    get_backend_apps_yw_pipeline,
+)
+from youwol.utils.context import Context
 
 
 class PipelineFactory(IPipelineFactory):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     async def get(self, env: YouwolEnvironment, context: Context):
-        return await get_backend_apps_yw_pipeline(name="assets-gateway", context=context)
+        return await get_backend_apps_yw_pipeline(
+            name="assets-gateway", context=context
+        )
